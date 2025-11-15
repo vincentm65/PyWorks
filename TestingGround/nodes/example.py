2def63850cdd48f37b72cb033b3533389af899fe
Vincent Miranda <vincentmiranda65@gmail.com>
2025-11-15 11:18:12 -0500

    Updated our examples

--- TestingGround/nodes/example.py
+++ TestingGround/nodes/example.py
@@ -8,25 +8,48 @@ def node(func):
     return func
 
 @node
-def send_example(inputs, global_state):
-    """
-    Example node that demonstrates the basic structure.
-
-    Args:
-        inputs: Dictionary of parent node outputs
-        global_state: Shared state dictionary
-
-    Returns:
-        Dictionary of outputs for child nodes
-    """
-    time.sleep(1)
+def send_local_example(inputs, global_state):
     message = 'Hello World!'
+    time.sleep(1)
+
+    # We return "results" in this case as our key, that has message as our value.
     return {'result': message}
 
 @node
-def receive_example(inputs, global_state):
+def receive_local_example(inputs, global_state):
+    # Below are all our of inputs we received as an argument
     print(f'All inputs {inputs}')
+
     time.sleep(1)
-    data = inputs.get('example.send_example', {}).get('result', '')
+
+    # We can get specific inputs with .get().
+    data = inputs.get('example.send_local_example', {}).get('result', '')
+
+    # Printing the data we got from the results of send_local_example
     print(f'Received: {data} from parent node')
-    return {'processed': data.upper()}
+
+    # No need for a return sice we are only receiving in this function.
+    return {}
+
+@node
+def create_global_example(inputs, global_state):
+
+    # Here we set a standard variable
+    this_is_global = "Global variable all nodes can read!"
+
+    # Then it is made global by assingning it a new value in the global state dict. Now all nodes can access it.
+    global_state["Test_Global"] = this_is_global
+    time.sleep(1)
+
+    # No need to return anything.
+    return {}
+
+@node
+def receive_global_example(inputs, global_state):
+
+    # Print out the global state, this will print anywhere after the global
+    print(global_state["Test_Global"])
+    time.sleep(1)
+    
+    # No need to return
+    return {}
